d5c425d06ea8cf1ae60b700c3eab18ee8b0a1c8c
Omar Yei Gaston Chalas <ogaston@adm.unapec.edu.do>
2019-10-28 15:01:33 -0400

    Primer Commit

---
+++ BusinessLogic/UserControl1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace BusinessLogic
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 272);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
