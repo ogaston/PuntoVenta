cbcfafdf675abc2651af7843590ac3b20a8a7d52
Michael Williams <michaeldrums23@gmail.com>
2019-10-29 08:55:58 -0400

    Configuracion de la Cadena de Conexion

---
+++ Data/ConectionDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Librerias Utilizadas
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Data
+{
+   public class ConectionDAL
+    {
+        private static SqlConnection Conexion;
+        public ConectionDAL()
+        {
+            Conexion = new SqlConnection(GetConection);
+        }
+
+        private string GetConection 
+        { 
+            get 
+            { 
+                return ConfigurationManager.ConnectionStrings["PosSystem.Properties.Settings.conexion"].ToString(); 
+            }
+        }
+
+        private static SqlConnection ConecctionString()
+        {
+            return Conexion;
+        }
+    }
+}
