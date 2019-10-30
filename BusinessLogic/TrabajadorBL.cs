78b6a9c15619c983d73e4dd5953167fe0e645baa
Michael Williams <michaeldrums23@gmail.com>
2019-10-30 10:39:29 -0400

    Capa Business Logic Hecha

---
+++ BusinessLogic/TrabajadorBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Data;
+using Entity;
+
+namespace BusinessLogic
+{
+    public class TrabajadorBL
+    {
+        public static void InsertTrabajador(Trabajador insertNew)
+        {
+            TrabajadorDAL.InsertTrabajador(insertNew);
+        }
+
+        public static void UpdateTrabajador(Trabajador update)
+        {
+            TrabajadorDAL.UpdateTrabajador(update);
+        }
+
+        public static void DeleteTrabajador(int id)
+        {
+            TrabajadorDAL.DeleteTrabajador(id);
+        }
+
+        public static DataTable SelectAllTrabajador()
+        {
+            return TrabajadorDAL.SelectAllTrabajador();
+        }
+    }
+}
