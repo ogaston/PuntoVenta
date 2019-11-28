d25b5ed49451577f4f265adcc31ff712e8a309e6
Michael Williams <michaeldrums23@gmail.com>
2019-11-28 19:50:02 -0400

    Modulo Presentacion Modificado

--- PosSystem/PresentacionForm.designer.cs
+++ PosSystem/PresentacionForm.designer.cs
@@ -70,6 +70,7 @@
             // 
             // txtDescripcionProducto
             // 
+            this.txtDescripcionProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtDescripcionProducto.Location = new System.Drawing.Point(157, 121);
             this.txtDescripcionProducto.Multiline = true;
             this.txtDescripcionProducto.Name = "txtDescripcionProducto";
@@ -88,9 +89,10 @@
             // 
             // txtNombreProducto
             // 
-            this.txtNombreProducto.Location = new System.Drawing.Point(171, 82);
+            this.txtNombreProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreProducto.Location = new System.Drawing.Point(158, 82);
             this.txtNombreProducto.Name = "txtNombreProducto";
-            this.txtNombreProducto.Size = new System.Drawing.Size(344, 20);
+            this.txtNombreProducto.Size = new System.Drawing.Size(344, 26);
             this.txtNombreProducto.TabIndex = 4;
             // 
             // label3
@@ -109,7 +111,7 @@
             this.LabelNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
             this.LabelNuevo.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.LabelNuevo.Name = "LabelNuevo";
-            this.LabelNuevo.Size = new System.Drawing.Size(0, 22);
+            this.LabelNuevo.Size = new System.Drawing.Size(0, 25);
             this.LabelNuevo.Text = "Nuevo";
             // 
             // BtnAgregar
@@ -117,7 +119,7 @@
             this.BtnAgregar.Image = ((System.Drawing.Image)(resources.GetObject("BtnAgregar.Image")));
             this.BtnAgregar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnAgregar.Name = "BtnAgregar";
-            this.BtnAgregar.Size = new System.Drawing.Size(69, 22);
+            this.BtnAgregar.Size = new System.Drawing.Size(86, 25);
             this.BtnAgregar.Text = "Agregar";
             this.BtnAgregar.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
@@ -126,7 +128,7 @@
             this.BtnActualizar.Image = ((System.Drawing.Image)(resources.GetObject("BtnActualizar.Image")));
             this.BtnActualizar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnActualizar.Name = "BtnActualizar";
-            this.BtnActualizar.Size = new System.Drawing.Size(79, 22);
+            this.BtnActualizar.Size = new System.Drawing.Size(98, 25);
             this.BtnActualizar.Text = "Actualizar";
             this.BtnActualizar.Click += new System.EventHandler(this.BtnActualizar_Click);
             // 
@@ -135,12 +137,13 @@
             this.BtnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("BtnEliminar.Image")));
             this.BtnEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnEliminar.Name = "BtnEliminar";
-            this.BtnEliminar.Size = new System.Drawing.Size(70, 22);
+            this.BtnEliminar.Size = new System.Drawing.Size(87, 25);
             this.BtnEliminar.Text = "Eliminar";
             this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.LabelNuevo,
             this.BtnNuevo,
@@ -149,7 +152,7 @@
             this.BtnEliminar});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(737, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(709, 28);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -158,7 +161,7 @@
             this.BtnNuevo.Image = ((System.Drawing.Image)(resources.GetObject("BtnNuevo.Image")));
             this.BtnNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.BtnNuevo.Name = "BtnNuevo";
-            this.BtnNuevo.Size = new System.Drawing.Size(62, 22);
+            this.BtnNuevo.Size = new System.Drawing.Size(76, 25);
             this.BtnNuevo.Text = "Nuevo";
             this.BtnNuevo.Click += new System.EventHandler(this.BtnNuevo_Click);
             // 
@@ -168,22 +171,23 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.tabControl1.Location = new System.Drawing.Point(0, 39);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(725, 337);
+            this.tabControl1.Size = new System.Drawing.Size(701, 388);
             this.tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.groupBox1);
             this.tabPage1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(717, 311);
+            this.tabPage1.Size = new System.Drawing.Size(693, 355);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Mantenimiento";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -195,17 +199,17 @@
             this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Controls.Add(this.txtBusqueda);
             this.tabPage2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(717, 311);
+            this.tabPage2.Size = new System.Drawing.Size(693, 355);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Busqueda";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // BtnBuscar
             // 
-            this.BtnBuscar.Location = new System.Drawing.Point(274, 43);
+            this.BtnBuscar.Location = new System.Drawing.Point(309, 47);
             this.BtnBuscar.Name = "BtnBuscar";
             this.BtnBuscar.Size = new System.Drawing.Size(124, 35);
             this.BtnBuscar.TabIndex = 5;
@@ -216,7 +220,7 @@
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.CornflowerBlue;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 84);
+            this.dataGridView1.Location = new System.Drawing.Point(56, 88);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(583, 221);
@@ -227,7 +231,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Location = new System.Drawing.Point(63, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(184, 24);
             this.label1.TabIndex = 3;
@@ -235,9 +239,9 @@
             // 
             // txtBusqueda
             // 
-            this.txtBusqueda.Location = new System.Drawing.Point(21, 51);
+            this.txtBusqueda.Location = new System.Drawing.Point(56, 55);
             this.txtBusqueda.Name = "txtBusqueda";
-            this.txtBusqueda.Size = new System.Drawing.Size(247, 20);
+            this.txtBusqueda.Size = new System.Drawing.Size(247, 26);
             this.txtBusqueda.TabIndex = 2;
             this.txtBusqueda.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBusqueda_KeyPress);
             // 
@@ -245,14 +249,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(737, 399);
+            this.ClientSize = new System.Drawing.Size(709, 439);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PresentacionForm";
             this.ShowInTaskbar = false;
-            this.Text = "Presentacion de Articulo";
+            this.Text = "Formulario de Presentacion";
             this.Load += new System.EventHandler(this.PresentacionForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
